0136227dfd9c44b3e75b7622bd040056115f0224
udo-bit <enote_gxl@163.com>
2024-08-14 23:48:39 +0800

    router

---
+++ src/components/pro-table/index.tsx
@@ -0,0 +1,10 @@
+const proTable = defineComponent({
+    name: 'ProTable',
+    setup() {
+        return () => {
+            return "hello,world"
+        }
+    }
+})
+
+export default proTable
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,33 +1,10 @@
 <script lang="ts" setup>
-
-
-const onRequest1 = () => {
-  useGet('/').then(res => {
-    console.log(res)
-  })
-}
-const onRequest2 = () => {
-  useGet('/403').then(res => {
-    console.log(res)
-  })
-}
+import ProTable from "~/components/pro-table/index.tsx";
 
 </script>
 
 <template>
-  <div>
-
-    <n-space>
-      <n-button @click="onRequest1">
-        200
-      </n-button>
-
-      <n-button @click="onRequest2">
-        403
-      </n-button>
-
-    </n-space>
-  </div>
+  <ProTable/>
 </template>
 
 <style scoped>
